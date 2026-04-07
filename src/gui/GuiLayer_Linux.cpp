6092a36eff05554bf650d8b67ec8630130a78c82
DiSHTiX <26351298+DiSHTiX@users.noreply.github.com>
2026-04-08 07:11:54 +0800

    - Introduced new utility functions `SkipRow` and `AdvanceRow` in GuiWidgets and Removed redundant code from GuiLayer_Common. - Updated the GUI layout to utilize these new functions, improving the overall structure of the code. - Tweaks to GUI structure

--- src/gui/GuiLayer_Linux.cpp
+++ src/gui/GuiLayer_Linux.cpp
@@ -151,6 +151,7 @@ bool GuiLayer::Init() {
 
     ImPlot::CreateContext();
     SetupGUIStyle();
+    SetupPlotStyle();
 
     ImGui_ImplGlfw_InitForOpenGL(g_window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
